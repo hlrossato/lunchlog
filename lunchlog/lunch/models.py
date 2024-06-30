a92565ee877ad13bd756fb327617d373c11eac6b
Higor Rossato <higorleandrorossato@gmail.com>
2024-06-30 15:42:35 +0200

    Adding User FK to Receipt model

--- lunchlog/lunch/models.py
+++ lunchlog/lunch/models.py
@@ -7,6 +7,7 @@ class Receipt(models.Model):
     price = models.DecimalField(verbose_name="Price", max_digits=5, decimal_places=2)
     restaurant_name = models.CharField(verbose_name="Restaurant", max_length=100)
     address = models.CharField(verbose_name="Address", max_length=250)
+    user = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "Receipt"
