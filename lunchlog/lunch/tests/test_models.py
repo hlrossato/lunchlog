606e333ca38c93b8b380c7f1a22fade7b12713a4
Higor Rossato <higorleandrorossato@gmail.com>
2024-07-05 00:37:28 +0200

    Trying to fix tests on CI

--- lunchlog/lunch/tests/test_models.py
+++ lunchlog/lunch/tests/test_models.py
@@ -1,8 +1,20 @@
 import pytest
+from unittest import mock
 from lunch.models import Receipt
+from google_places_api.api import GooglePlaceDetail
 
 
 @pytest.mark.django_db
-def test_receipt__created(receipt_data):
+@mock.patch("lunch.services.GooglePlacesAPI.place_details")
+@mock.patch("lunch.services.GooglePlacesAPI.find_place_id")
+def test_receipt__created(
+    mocked_place_id,
+    mocked_place_details,
+    receipt_data,
+    google_place_detail,
+):
+    mocked_place_id.return_value = "test"
+    mocked_place_details.return_value = GooglePlaceDetail(google_place_detail["result"])
+
     receipt = Receipt.objects.create(**receipt_data)
     assert receipt is not None
--- lunchlog/lunch/tests/test_serializers.py
+++ lunchlog/lunch/tests/test_serializers.py
@@ -1,6 +1,8 @@
 import pytest
+from unittest import mock
 from django.test.client import RequestFactory
 from lunch.api.serializers import ReceiptModelSerializer
+from google_places_api.api import GooglePlaceDetail
 
 
 @pytest.mark.django_db
@@ -10,7 +12,18 @@ def test_serializer(receipt_input_data, receipt_data):
 
 
 @pytest.mark.django_db
-def test_serializer__create(receipt_input_data, user):
+@mock.patch("lunch.services.GooglePlacesAPI.place_details")
+@mock.patch("lunch.services.GooglePlacesAPI.find_place_id")
+def test_serializer__create(
+    mocked_place_id,
+    mocked_place_details,
+    receipt_input_data,
+    user,
+    google_place_detail,
+):
+    mocked_place_id.return_value = "test"
+    mocked_place_details.return_value = GooglePlaceDetail(google_place_detail["result"])
+
     request = RequestFactory()
     request.user = user
     serializer = ReceiptModelSerializer(
